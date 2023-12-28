dbaf487f1d1bb1074386c9a6b31fc03c618576d0
P13RL <ambrosopierlorenzo@gmail.com>
2023-12-28 20:27:02 +0100

    Iniziato tabellone

--- Box.cpp
+++ Box.cpp
@@ -1,13 +1,4 @@
 #include "Box.h";
 class Box{
-    private:
-        int box_type_;
-        int id_box_;
-        int box_construction_;
-        int box_cost;
-
     
-    public:
-        Box::Box(int id) : id_box_{id}{};
-
 };
---
+++ Scoreboard.h
@@ -0,0 +1,16 @@
+#ifndef SCOREBOARD_H
+#define SCOREBOARD_H
+
+#include <vector>
+
+
+namespace Scoreboard{
+
+    class Scoreboard{
+
+        protected:
+           std::vector<Box> board;
+    };
+}
+
+#endif

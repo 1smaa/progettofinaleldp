331b9654d5c9cb48d2beb6795c0aae62f28a1942
P13RL <ambrosopierlorenzo@gmail.com>
2023-12-28 19:56:42 +0100

    Modifiche file box.h

--- Box.h
+++ Box.h
@@ -2,11 +2,47 @@
 #define FUNCTION_BOX_H
 
 namespace Box{
-    class Box{
-        public:
-        //Costruttori
-            Box(int id);
 
+    class Box{
+
+        protected:
+            //Definisce il tipo di casella: Economica, Standard, Lusso
+            int box_type_;
+
+            //Definisce l'id per identificare la casella nel tabellone
+            int id_box_;
+
+            //Definisce il tipo di costruzione presente sulla casella
+            int box_construction_;
+
+            //Definisce il costo associato alla casella
+            int box_cost_;
+
+        public:
+            //Costruttore di default
+            Box() : box_type_(0), id_box_(0), box_construction_(0), box_cost_(0){}; //DA VALUTARE??
+
+            //Costruttori con parametri    
+            Box(int id) : id_box_(id) {};
+            Box(int id, int tipo) : id_box_(id), box_type_(tipo){};
+            Box(int id, int tipo, int costruzioni) : id_box_(id), box_type_(tipo), box_construction_(costruzioni){};
+
+            //Costruttori di copia e spostamento
+            //Box(const Box&) = delete;
+
+
+            //Funzioni per settare le variabili protected
+            void setBoxType(int type){this->box_type_ = type; };
+            void setIdBox(int id){this->id_box_ = id; };
+            void setBoxConstruction(int construction){this->box_construction_ = construction;};
+            void setBoxCost(int cost){this->box_cost_ = cost; };
+
+            //Funzioni per return variabili protected
+
+            int getBoxType(){return this->box_type_;};
+            int getIdBox(){return this->id_box_;};
+            int getBoxConstruction(){return this-> box_construction_;};
+            int getBoxCost(){return this->box_cost_;};
 
         //Bozza di funzioni
             void print_box_info(); //Stampa le informazioni della casella
@@ -23,4 +59,4 @@ namespace Box{
 }
 
 
-#endif/*FUNCTION_CASELLA_H*/
+#endif/*FUNCTION_BOX_H*/

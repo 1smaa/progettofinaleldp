85a809087f6f9338e051041fda18fab18aee0e03
P13RL <ambrosopierlorenzo@gmail.com>
2023-12-29 14:41:03 +0100

    Implementazione classe Box.cpp

--- Box.cpp
+++ Box.cpp
@@ -1,4 +1,142 @@
 #include "Box.h";
-class Box{
+#include <iostream>
+/*
+Il tipo di casella (box_type_) è specificato da questi numeri:
+0 = Partenza
+1 = Angolare
+2 = Economica 6€
+3 = Standard 10€
+4 = Lusso 20€
+
+Le costruzioni (box_construction_)invece sono specificate da:
+0 = Terreno
+1 = Casa
+2 = Albergo
+
+box_cost_ = costo per acquisto casella 
+box_stay_cost_ = costo pernottamento
+box_construction_ = tipo di costruzione presente sulla casella
+*/
+
+//Implementazione costruttori
+Box::Box(int id){
+    this->box_type_ = 0;
+    this->box_cost_ = 0;
+}
+
+Box::Box(int id, int tipo){
+    this->box_construction_ = 0; //Inizialmente ogni casella rappresenta un terreno acquistabile
+    //Assegnamento prezzo per acquisto
+    this-> box_cost_ = assegnaCosto(this->box_type_);
+}
+
+Box::Box(int id, int tipo, int costruzioni){
+    //Controllo il prezzo da assegnare alla casella, se non sono presenti costruzioni
+    this->box_cost_ = assegnaCosto(this->box_type_);
+    //Controllo il prezzo da assegnare al pernottamento, se sono presenti costruzioni
+    this->box_stay_cost_ = assegnaPernottamento(this->box_type_, this->box_construction_);
+}
+
+void Box::print_box_info(){
+    std::string tipo_casella;
+    std::string costruzioni_casella;
+
+    //Switch-case per trovare il tipo di casella da stampare nel messaggio
+    switch(this->box_type_){
+        case 0:
+            tipo_casella = "Partenza";
+            break;
+        case 1:
+            tipo_casella = "Angolare";
+            break;
+        case 2:
+            tipo_casella = "Economica";
+            break;
+        case 3:
+            tipo_casella = "Standard";
+            break;
+        case 4:
+            tipo_casella = "Lusso";
+            break;
+    }
+
+    //Switch-case per trovare il tipo di costruzione presente sulla casella
+    switch(this->box_construction_){
+        case 0:
+            costruzioni_casella = "Terreno";
+            break;
+        case 1:
+            costruzioni_casella = "Casa";
+            break;
+        case 2:
+            costruzioni_casella = "Albergo";
+            break;
+    }
+
+    std::cout<< "Casella numero: "<< this->box_id_<<'\n'<<"Tipo casella: "<<tipo_casella<<'\n'<<"Costruzioni casella:"<<costruzioni_casella<<'\n'<<"Costo casella: "<<this->box_cost_<<'\n'<<"Costo pernottamento: "<<this->box_stay_cost_<<std::endl;
+}
+
+bool Box::build_on_box(){
+    //Controllo che sulla casella non sia già presente un albergo, in tal caso non posso costruire
+    if(this->box_construction_ == 2) return false;
     
-};
+    //Da Terreno passo a Casa
+    if(this->box_construction_ == 0){
+        this->box_construction_ = 1;
+        this-> box_stay_cost_ = assegnaPernottamento(this->box_type_, this->box_construction_);
+        return true;
+    }
+
+    //Da casa passo ad Albergo
+    if(this->box_construction_ == 1){
+        this->box_construction_ = 2;
+        this->box_stay_cost_ = assegnaPernottamento(this->box_type_, this->box_construction_);
+        return true;
+    }
+}
+
+
+//Funzioni che restituisce il tipo di costruzione presente sulla casella
+std::string Box::construction(){
+    switch(this->box_construction_){
+        case 0:
+            return "Terreno";
+            break;
+        case 1:
+            return "Casa";
+            break;
+        case 2:
+            return "Albergo";
+            break;
+    }
+
+}
+
+//Funzioni ausiliare
+
+//Funzione per assegnare il costo ad una casella in base al tipo
+int assegnaCosto(int tipo_casella_){
+    //Se la casella è un terreno assegno i costi associati all'acquisto della casella
+        if(tipo_casella_ = 2) return 6;
+        else if(tipo_casella_= 3) return 10;
+        else if(tipo_casella_ = 4) return 20;
+        else return 0; //La casella è la partenza o è una casella angolare (quindi non acquistabili)
+}
+
+//Funzione per assegnare il costo del pernottamento in base al tipo di casella e alla struttura costruita sopra
+int assegnaPernottamento(int tipo_casella_, int costruzioni_casella_){
+        //Se nella casella è presente una casa (=1) associo i costi del pernottamento
+    if(costruzioni_casella_=1) 
+        if(tipo_casella_ = 2) return 2;
+        else if(tipo_casella_ = 3) return 4;
+        else return 7;
+
+    //Altrimenti se è presente un castello (=2) associo i costi del pernottamento
+    else{
+        if(tipo_casella_= 2) return 4;
+        else if(tipo_casella_= 3) return 8;
+        else return 14;
+    }
+
+    return 0; //nel caso in cui non fossero presenti costruzioni il pernottamento è uguale a 0
+}
--- Box.h
+++ Box.h
@@ -1,16 +1,48 @@
 #ifndef FUNCTION_BOX_H
 #define FUNCTION_BOX_H
-
-namespace Box{
-
     class Box{
 
-        protected:
+        public:
+            //Costruttore di default
+            // Box() : box_type_(0), id_box_(0), box_construction_(0), box_cost_(0){}; //DA VALUTARE??
+
+            //Costruttori con parametri    
+            Box(int id): box_id_(id){}
+            Box(int id, int tipo) : box_id_(id), box_type_(tipo), box_stay_cost_(0){}
+            Box(int id, int tipo, int costruzioni) : box_id_(id), box_type_(tipo), box_construction_(costruzioni){}
+
+            //Costruttori di copia e spostamento
+            //Box(const Box&) = delete;
+
+
+            //Funzioni per settare le variabili protected
+            void setBoxType(int type){this->box_type_ = type; }
+            void setIdBox(int id){this->box_id_ = id; }
+            void setBoxConstruction(int construction){this->box_construction_ = construction;}
+            void setBoxCost(int cost){this->box_cost_ = cost; }
+
+            //Funzioni per return variabili protected
+
+            int getBoxType(){return this->box_type_;}
+            int getIdBox(){return this->box_id_;}
+            int getBoxConstruction(){return this-> box_construction_;}
+            int getBoxCost(){return this->box_cost_;}
+
+        //Bozza di funzioni
+            void print_box_info(); //Stampa le informazioni della casella
+
+            bool build_on_box(); //Costruisce su una casella e ritorna un valore booleano
+
+            int pernottamento(){return this->box_stay_cost_;}   //Ritorna il costo del pernottamento
+
+            std::string construction(); //Ritorna le costruzioni presenti sulla casella
+
+        private:
             //Definisce il tipo di casella: Economica, Standard, Lusso
             int box_type_;
 
             //Definisce l'id per identificare la casella nel tabellone
-            int id_box_;
+            int box_id_;
 
             //Definisce il tipo di costruzione presente sulla casella
             int box_construction_;
@@ -18,43 +50,10 @@ namespace Box{
             //Definisce il costo associato alla casella
             int box_cost_;
 
-        public:
-            //Costruttore di default
-            Box() : box_type_(0), id_box_(0), box_construction_(0), box_cost_(0){}; //DA VALUTARE??
-
-            //Costruttori con parametri    
-            Box(int id) : id_box_(id) {};
-            Box(int id, int tipo) : id_box_(id), box_type_(tipo){};
-            Box(int id, int tipo, int costruzioni) : id_box_(id), box_type_(tipo), box_construction_(costruzioni){};
-
-            //Costruttori di copia e spostamento
-            //Box(const Box&) = delete;
-
-
-            //Funzioni per settare le variabili protected
-            void setBoxType(int type){this->box_type_ = type; };
-            void setIdBox(int id){this->id_box_ = id; };
-            void setBoxConstruction(int construction){this->box_construction_ = construction;};
-            void setBoxCost(int cost){this->box_cost_ = cost; };
-
-            //Funzioni per return variabili protected
-
-            int getBoxType(){return this->box_type_;};
-            int getIdBox(){return this->id_box_;};
-            int getBoxConstruction(){return this-> box_construction_;};
-            int getBoxCost(){return this->box_cost_;};
-
-        //Bozza di funzioni
-            void print_box_info(); //Stampa le informazioni della casella
-
-            bool build_on_box(); //Costruisce su una casella e ritorna un valore booleano
-
-            int pernottamento();   //Ritorna il costo del pernottamento
-
-            int costruzioni(); //Ritorna le costruzioni presenti sulla casella
-
+            //Costo associato al pernottamento
+            int box_stay_cost_;
     };
-}
+
 
 
 #endif/*FUNCTION_BOX_H*/

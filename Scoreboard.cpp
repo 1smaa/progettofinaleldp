240dc5d2bb0c8692772a42edc4d6cd8f67c8578e
P13RL <ambrosopierlorenzo@gmail.com>
2024-01-02 16:06:36 +0100

    Scoreboard

--- Scoreboard.cpp
+++ Scoreboard.cpp
@@ -76,7 +76,7 @@ std::string Scoreboard::print_scoreboard(){
 
     //stream_string<<std::setfill(' '); //Così che vengano fuori gli spazi nella casella vuota
     //Prima riga di stampa (coordinate numeriche)
-    stream_string   <<std::setw(LARGHEZZA_CASELLA_STAMPA)<<" "
+    stream_string   <<std::setw(LARGHEZZA_CASELLA_STAMPA)<<"0"
                     <<std::setw(LARGHEZZA_CASELLA_STAMPA)<< "1"
                     <<std::setw(LARGHEZZA_CASELLA_STAMPA)<< "2"
                     <<std::setw(LARGHEZZA_CASELLA_STAMPA)<< "3"
@@ -88,7 +88,7 @@ std::string Scoreboard::print_scoreboard(){
     
 
     //Prima riga effettiva del tabellone
-    stream_string <<std::setw(LARGHEZZA_CASELLA_STAMPA)<<"A";
+    stream_string<<"A";
     for(int i=14; i<=21; i++){
         if(i==14 || i == 21){
             stream_string   <<std::setw(LARGHEZZA_CASELLA_STAMPA)<<"||";
@@ -98,9 +98,10 @@ std::string Scoreboard::print_scoreboard(){
             stream_string <<std::setw(LARGHEZZA_CASELLA_STAMPA)<<"|"<<board[i]->getBoxType()<<"|";
         }
     }
+    stream_string <<std::endl;
     
     //Seconda riga del tabellone
-    stream_string <<std::setw(LARGHEZZA_CASELLA_STAMPA)<<"B";
+    stream_string <<"B";
     stream_string <<std::setw(LARGHEZZA_CASELLA_STAMPA)<<"|"<<board[13]->getBoxType()<<"|";
     for(int i=0; i<6; i++){
          stream_string <<std::setw(LARGHEZZA_CASELLA_STAMPA)<<std::setfill(' ');
@@ -109,7 +110,7 @@ std::string Scoreboard::print_scoreboard(){
 
     
     //Terza riga del tabellone
-    stream_string <<std::setw(LARGHEZZA_CASELLA_STAMPA)<<"C";
+    stream_string <<"C";
     stream_string <<std::setw(LARGHEZZA_CASELLA_STAMPA)<<"|"<<board[12]->getBoxType()<<"|";
     for(int i=0; i<6; i++){
          stream_string <<std::setw(LARGHEZZA_CASELLA_STAMPA)<<std::setfill(' ');
@@ -117,7 +118,7 @@ std::string Scoreboard::print_scoreboard(){
     stream_string <<std::setw(LARGHEZZA_CASELLA_STAMPA)<<"|"<<board[23]->getBoxType()<<"|"<<std::endl;
 
     //Quarta riga del tabellone
-    stream_string <<std::setw(LARGHEZZA_CASELLA_STAMPA)<<"D";
+    stream_string <<"D";
     stream_string <<std::setw(LARGHEZZA_CASELLA_STAMPA)<<"|"<<board[11]->getBoxType()<<"|";
     for(int i=0; i<6; i++){
          stream_string <<std::setw(LARGHEZZA_CASELLA_STAMPA)<<std::setfill(' ');
@@ -125,7 +126,7 @@ std::string Scoreboard::print_scoreboard(){
     stream_string <<std::setw(LARGHEZZA_CASELLA_STAMPA)<<"|"<<board[24]->getBoxType()<<"|"<<std::endl;
 
     //Quinta riga del tabellone
-    stream_string <<std::setw(LARGHEZZA_CASELLA_STAMPA)<<"E";
+    stream_string <<"E";
     stream_string <<std::setw(LARGHEZZA_CASELLA_STAMPA)<<"|"<<board[10]->getBoxType()<<"|";
     for(int i=0; i<6; i++){
          stream_string <<std::setw(LARGHEZZA_CASELLA_STAMPA)<<std::setfill(' ');
@@ -133,7 +134,7 @@ std::string Scoreboard::print_scoreboard(){
     stream_string <<std::setw(LARGHEZZA_CASELLA_STAMPA)<<"|"<<board[25]->getBoxType()<<"|"<<std::endl;
 
     //Sesta riga del tabellone
-    stream_string <<std::setw(LARGHEZZA_CASELLA_STAMPA)<<"F";
+    stream_string <<"F";
     stream_string <<std::setw(LARGHEZZA_CASELLA_STAMPA)<<"|"<<board[9]->getBoxType()<<"|";
     for(int i=0; i<6; i++){
          stream_string <<std::setw(LARGHEZZA_CASELLA_STAMPA)<<std::setfill(' ');
@@ -141,7 +142,7 @@ std::string Scoreboard::print_scoreboard(){
     stream_string <<std::setw(LARGHEZZA_CASELLA_STAMPA)<<"|"<<board[26]->getBoxType()<<"|"<<std::endl;
 
     //Settima riga del tabellone
-    stream_string <<std::setw(LARGHEZZA_CASELLA_STAMPA)<<"G";
+    stream_string<<"G";
     stream_string <<std::setw(LARGHEZZA_CASELLA_STAMPA)<<"|"<<board[8]->getBoxType()<<"|";
     for(int i=0; i<6; i++){
          stream_string <<std::setw(LARGHEZZA_CASELLA_STAMPA)<<std::setfill(' ');
@@ -149,10 +150,10 @@ std::string Scoreboard::print_scoreboard(){
     stream_string <<std::setw(LARGHEZZA_CASELLA_STAMPA)<<"|"<<board[27]->getBoxType()<<"|"<<std::endl;
 
     //Ottava riga del tabellone
-    stream_string <<std::setw(LARGHEZZA_CASELLA_STAMPA)<<"H";
+    stream_string<<"H";
     for(int i=7; i>=0; i--){
         if(i==7){
-            stream_string   <<std::setw(LARGHEZZA_CASELLA_STAMPA)<<"||";
+            stream_string<<std::setw(LARGHEZZA_CASELLA_STAMPA)<<"||";
         }
         else{
             //Ricavo il tipo di casella, PARTENZA, ECONOMICA, STANDARD, LUSSO

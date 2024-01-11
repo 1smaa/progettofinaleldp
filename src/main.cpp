b4994e0e41c8018a64d5ff25bcce28403331f66e
irenecian <94189239+irenecian@users.noreply.github.com>
2024-01-11 14:08:42 +0100

    nuovo main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -1,31 +1,49 @@
+#include <vector>
+#include <iostream>
+#include <fstream>
+
 #include "Dadi.h"
 #include "Partita.h"
 #include "Board.h"
 
-
-#include <vector>
-
 #define DEFAULT_SALDO 100
 
-int main(void){
+int main(){
+
+//creo il tabellone e i giocatori
     std::vector<Board::Player*> giocatori{};
-    for(int i=0;i<4;i++){
-        giocatori.push_back(new Board::Player(true,i,DEFAULT_SALDO));
-    }
-    //giocatori.push_back(new Board::Player(false,3,DEFAULT_SALDO));
     Board::Board tabellone(giocatori);
-    Partita p(giocatori,&tabellone);
-    int t=p.inizia();
-    while(!p.terminata()){
-        std::cout<<t<<std::endl;
-        t=p.prossimoTurno();
+        //inserisco nel vettore i giocatori e inizializzo il saldo di ognuno
+        for(int i=0; i<4; i++){
+        giocatori.push_back(new Board::Player(true, i, DEFAULT_SALDO));
     }
-    std::vector<Board::Player*> inPLay=tabellone.getPlayers();
-    for(int i=0;i<inPLay.size();i++){
-        std::cout<<inPLay[i]->getSaldo()<<std::endl;
+//apro il file di testo
+    std::ofstream outputFile ("MONOPOLY.txt"); 
+        if (!outputFile.is_open()){ //verifico sia aperto correttamente
+            std::cerr << "errore nell'apertura del file" << std::endl;
+            return 1;
+        }
+//creo un oggetto partita
+    Partita partita(giocatori, &tabellone);
+    int g = partita.inizia(); //inizio la partita
+    outputFile << "PARTITA INIZIATA! Il primo giocatore è " << giocatori[0] << std::endl;
+    outputFile << partita.log() <<std::endl;
+        while (!partita.terminata()){
+            std::cout << g << std::endl;
+            g= partita.prossimoTurno(); //esegui il turno e passa al giocatore successivo
+        }
+
+    std::vector<Board::Player*> inPLay= tabellone.getPlayers();
+        for(int i=0; i < inPLay.size(); i++){
+        std::cout<< inPLay[i]-> getSaldo()<< std::endl;
     }
-    std::vector<Board::Player> vincitori=p.vincitore();
-    for(int i=0;i<vincitori.size();i++){
-        std::cout<<"Il giocatore "<<vincitori[i].getId()<<" ha vinto"<<std::endl;
+//stampo il vincitore
+    std::vector<Board::Player> vincitori = partita.vincitore();
+        for(int i=0; i < vincitori.size(); i++){
+        std::cout << "PARTITA TERMINATA! Il giocatore " << vincitori[i].getId() << " ha vinto" << std::endl;
     }
-}
+//chiusura file
+    outputFile.close();
+    
+    return 0;
+} 
